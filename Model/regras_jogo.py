5e6126d425305e3e5e66d784bc1f4d6c0a8d4ca6
Guilherme Vassallo <guilhermevassallo09@gmail.com>
2020-11-04 18:24:35 -0300

    Mais padronizacao do codigo

--- Model/regras_jogo.py
+++ Model/regras_jogo.py
@@ -1,9 +1,5 @@
-from itertools import cycle
 from enum import Enum
-
-lista_peoes = [[0, 0, 0, 0], [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]]
-peoes_iniciais = [4, 4, 4, 4]
-
+import random
 
 class Cor(Enum):
     vermelho = 0
@@ -12,8 +8,16 @@ class Cor(Enum):
     azul = 3
 
 
+global vez, lista_casas, retasfinais, bases, pontuacao_jogadores, lista_peoes, peoes_iniciais
+
+
 def novo_jogo():  # cria tudo necessário para o começo de uma partida
-    global vez, casas, lista_casas, retasfinais, bases, jogadores
+    global vez, lista_casas, retasfinais, bases, pontuacao_jogadores, lista_peoes, peoes_iniciais
+
+    lista_peoes = [[0, 0, 0, 0], [1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]]
+    peoes_iniciais = [4, 4, 4, 4]
+
+    pontuacao_jogadores = [0, 0, 0, 0]
 
     vez = -1
     lista_casas = list()
@@ -31,28 +35,24 @@ def novo_jogo():  # cria tudo necessário para o começo de uma partida
             casa['tipo'] = 'comum'
         lista_casas.append(casa.copy())
 
-    casas = cycle(lista_casas)  # fazendo lista circular de casas
-
     for item in lista_casas:
         print(item)
 
-    jogadores = list()
     for i in range(4):
         retafinal = list()
 
-        for i in range(6):
-            casa.clear()
+        for _ in range(6):
+            casa = dict()
             casa['peoes'] = list()
             retafinal.append(casa.copy())
 
         retasfinais.append(retafinal.copy())
 
-    for i in range(retasfinais.count(retafinal)):
         print(retasfinais[i], i, Cor(i).name)
 
 
 def passar_a_vez():
-    global vez, casas, lista_casas, retasfinais, bases, jogadores
+    global vez, lista_casas, retasfinais, bases, pontuacao_jogadores, lista_peoes, peoes_iniciais
 
     if vez >= 3:
         vez = 0
@@ -63,7 +63,7 @@ def passar_a_vez():
 
 
 def posicionar_na_saida(peao):
-    global vez, casas, lista_casas, retasfinais, bases, jogadores
+    global vez, lista_casas, retasfinais, bases, pontuacao_jogadores, lista_peoes, peoes_iniciais
 
     lista_casas[13 * peao]['peoes'].append(peao)
     # OBS: Os números das casas de saída são múltiplos de 13
@@ -73,11 +73,20 @@ def posicionar_na_saida(peao):
 
 
 def posicionar(num_casa, cor_peao):
+    global vez, lista_casas, retasfinais, bases, pontuacao_jogadores, lista_peoes, peoes_iniciais
+
+    peao_na_casa = int()
+
+    if len(lista_casas[num_casa]['peoes']) > 0:
+        peao_na_casa = lista_casas[num_casa]['peoes'][0]
+
+    if peao_na_casa and peao_na_casa != cor_peao and lista_casas[num_casa]['tipo'] != 'abrigo':
+        captura(peao_na_casa, num_casa)
+
     lista_casas[num_casa]['peoes'].append(cor_peao)
 
-
 def movimentar(casa_origem, cor_peao, movimentos):
-    global vez, casas, lista_casas, retasfinais, bases, jogadores
+    global vez, lista_casas, retasfinais, bases, pontuacao_jogadores, lista_peoes, peoes_iniciais
 
     if cor_peao in lista_casas[casa_origem]['peoes']:
         lista_casas[casa_origem]['peoes'].remove(cor_peao)
@@ -90,7 +99,7 @@ def movimentar(casa_origem, cor_peao, movimentos):
     while movimentos > 0:
 
         # REGRA DO 6: No caso de tirar 6 no dado, salvar a ultima posição e a cor do peão movimentado
-            # Caso saia 6 pela terceira vez, pegar esse peão e trazê-lo de volta a casa inicial
+        # Caso saia 6 pela terceira vez, pegar esse peão e trazê-lo de volta a casa inicial
 
         # TODO: verificar se peão precisa ir para a reta final
         # TODO: se sim, chamar função de posicionamento na reta final, passando os movimentos restantes como parâmetro
@@ -106,12 +115,15 @@ def movimentar(casa_origem, cor_peao, movimentos):
         movimentos -= 1
         casas_percorridas += 1
 
-        # TODO: somar o número de casas percorridas ao score do jogador da cor do peão, para definir colocações ao final da partida
+        # TODO: somar o número de casas percorridas ao score do jogador da cor do peão
+        # para definir colocações ao final da partida
 
     posicionar(casa_origem, cor_peao)
 
 
 def abrigo(posicao, peao):
+    global vez, lista_casas, retasfinais, bases, pontuacao_jogadores, lista_peoes, peoes_iniciais
+
     if len(lista_casas[posicao]['peoes']) < 2:
         lista_casas[posicao]['peoes'].append(peao)
     else:
@@ -120,6 +132,8 @@ def abrigo(posicao, peao):
 
 
 def voltar_ao_inicio(peao):
+    global vez, lista_casas, retasfinais, bases, pontuacao_jogadores, lista_peoes, peoes_iniciais
+
     if peoes_iniciais[peao] == 4:
         print("Casas cheias")
     else:
@@ -127,6 +141,8 @@ def voltar_ao_inicio(peao):
 
 
 def retirar_do_inicio(peao):
+    global vez, lista_casas, retasfinais, bases, pontuacao_jogadores, lista_peoes, peoes_iniciais
+
     if peoes_iniciais[peao] == 0:
         print("Casas estão vazias")
     else:
@@ -134,19 +150,66 @@ def retirar_do_inicio(peao):
 
 
 def captura(peao, posicao):
-    global vez, casas, lista_casas, retasfinais, bases, jogadores
+    global vez, lista_casas, retasfinais, bases, pontuacao_jogadores, lista_peoes, peoes_iniciais
 
     if len(lista_casas[posicao]['peoes']) == 0:
         print("Vazia")
         return
     else:
         b = lista_casas[posicao]['peoes'].pop(0)
-        print(b)
-        # lista_casas['peoes'].clear()
-        lista_casas[posicao]['peoes'].append(peao)
+        print('Capturou:', b)
         voltar_ao_inicio(b)
-        exit(0)
+        return
 
+
+def rolardados():
+    global rolar, resul1, resul2, result3, TURN, rolagens, indice, ultimo
+
+    rolar = rolar + 1  # vendo inicio da rolagem, e o turno do jogadador da vez
+    if rolar == 1:
+        result1 = random.randint(1, 6)  # sortear numero de 1 a 6
+        print("Resultado da Primeira Rolagem", result1)  # mostrar numero sorteado
+        rolagens.append(result1)  # o valor sorteado entra na lista de rolagens
+        # movimentar(casa_origem, cor_peao, result1) #chamar a função de movimento recebendo o número sorteado, casa inicial e cor do peao
+        if result1 != 6:  # se numero sorteado for diferente de 6
+            rolar = 0  # variavel de rolagem é limpa
+            vez = False  # vez é passado para o próximo jogador
+        rolar = rolar + 1
+    # se result1 for 6, rolar dado novamente
+    if rolar == 2:
+        if result1 == 6:
+            result2 = random.randint(1, 6)  # sortear numero de 1 a 6
+            print("Resultado da Segunda Rolagem", result2)  # mostrar numero sorteado
+            rolagens.append(result2)  # o valor sorteado entra na lista de rolagens
+            # movimentar(casa_origem, cor_peao, result2) #chamar a função de movimento recebendo o número sorteado, casa inicial e cor do peao
+            if result2 != 6:  # se numero sorteado for diferente de 6
+                rolar = 0  # variavel de rolagem é limpa
+                vez = False  # vez é passado para o próximo jogador
+            rolar = rolar + 1
+    # se result2 for 6, rolar dado novamente
+    if rolar == 3:
+        if result2 == 6:
+            result3 = random.randint(1, 6)  # sortear numero de 1 a 6
+            print("Resultado da Terceira Rolagem", result3)  # mostrar numero sorteado
+            rolagens.append(result3)  # o valor sorteado entra na lista de rolagens
+            if result3 != 6:  # se numero sorteado for diferente de 6
+                # movimentar(casa_origem, cor_peao, result3) #chamar a função de movimento recebendo o número sorteado, casa inicial e cor do peao
+                rolar = 0  # variavel de rolagem é limpa
+                vez = False  # vez é passado para o próximo jogador
+            if result3 == 6:
+                indice = len(
+                    lista_peoes) - 1  # acessa o tamanho da lista e -1 para obter o indice do ultimo elemento da lista
+                ultimo = lista[indice]  # acessa o ultimo item da lista
+                print("Último Peão Moviemntado", ultimo)
+                # 15 nesse exemplo que criei
+                if ultimo in retafinal():  # se o ultimo peao movimentado estiver na lista de reta final(nada acontece)
+                    rolar = 0  # variavel de rolagem é limpa
+                    vez = False  # passa a vez
+                # caso contrario
+                posicionar_no_inicio(lista, vez, inicial)
+                # chama funcao movimento que move ultimo peao pra casa inicial
+                # inicia peoes recebe peoesclicacos[ultimo](???)
+                # casainicial.append(peoesclicados[ultimo])#não acho que seja assim
 
 # ---------------------------TESTES--------------------------------------------
 
@@ -163,6 +226,8 @@ movimentar(0, 0, 5)
 posicionar(10, 3)
 
 movimentar(10, 3, 2)
+
+movimentar(5, 0, 7)
 
 movimentar(20, lista_peoes[0][1], 5)
 
